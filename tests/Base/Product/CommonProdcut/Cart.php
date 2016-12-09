0a6282dc2a3d9d3196e96fe98a269fa648164d39
test <1060656096@qq.com>
2016-12-09 10:02:46 +0800

    aaa: 1. 修改CommonProduct产品类 2. 修改CommonProduct购物车类 3. 修改产品异常类 4. 增加购物车单元测试类

--- tests/Base/Product/CommonProdcut/Cart.php
+++ tests/Base/Product/CommonProdcut/Cart.php
@@ -1,7 +1,64 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2016/12/7
- * Time: 9:29
- */
+namespace Smile\PHPUnitTest\Tests\Demo\Product\CommonProduct;
+
+
+
+use Smile\PHPUnitTest\Tests\Base\Product\Product;
+use Smile\PHPUnitTest\Tests\Demo\Product\Exeption\ProductException;
+
+class Cart implements CartInterface{
+    protected $data     = null;
+    /**
+     * @var Product
+     */
+    protected $product  = null;
+    protected $num      = 0;
+    protected $totalMoney = 0;
+    protected $payMoney = 0;
+    /**
+     * 增加产品
+     * @param Product $product 产品
+     * @param integer $num 数量
+     * @return boolean
+     */
+    public function addProduct(Product $product, $num){
+        $this->product = $product;
+        $this->num     = $num;
+        $this->totalMoney = $this->product->getPrice() * $this->num;
+        $this->payMoney=$this->totalMoney;
+        return true;
+    }
+
+    /**
+     * 支付总金额
+     * @return int
+     */
+    public function getTotalMoney(){
+        return $this->totalMoney;
+    }
+
+    /**
+     * 实际支付金额
+     * @return int
+     */
+    public function getPayMoney(){
+        return $this->payMoney;
+    }
+
+    /**
+     * 结算
+     */
+    public function settle(){
+
+        if(!$this->product->isCheckType()){
+            throw new ProductException(ProductException::TYPE_ERROR);//不是普通商品
+        }
+        if($this->product->getQuantity() < 1){
+            throw new ProductException(ProductException::NOT_QUANTITY);//没有库存
+        }
+        if($this->product->getQuantity() < $this->num){
+            throw new ProductException(ProductException::LACK_QUANTITY);//库存不足
+        }
+
+    }
+}
--- tests/Base/Product/Exception/ProductException.php
+++ tests/Base/Product/Exception/ProductException.php
@@ -2,18 +2,24 @@
 namespace Smile\PHPUnitTest\Tests\Demo\Product\Exeption;
 
 class ProductException extends \Exception{
+
     /**
      * 产品不存在
      */
-    const NOT_EXIST = 1;
+    const NOT_EXIST     = 1;
     /**
      * 没有库存
      */
-    const NOT_QUANTITY = 2;
+    const NOT_QUANTITY  = 2;
     /**
      * 库存不足
      */
     const LACK_QUANTITY = 3;
+
+    /**
+     * 产品类型错误
+     */
+    const TYPE_ERROR    = 4;
 
     public function __construct($message, $code, Exception $previous)
     {
