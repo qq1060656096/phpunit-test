293f251743f0b097cec68db3e8ecef03475d55c3
test <1060656096@qq.com>
2016-12-09 16:36:02 +0800

    cca: 增加CartException.php注释

--- tests/Base/Product/Exception/CartException.php
+++ tests/Base/Product/Exception/CartException.php
@@ -1,9 +1,13 @@
 <?php
-namespace Smile\PHPUnitTest\Tests\Demo\Product\Exeption;
+namespace Smile\PHPUnitTest\Tests\Base\Product\Exception;
 
 class CartException extends \Exception{
     /**
-     * 不是整数
+     * 购买数量不是整数
      */
-    const NUM_NOT_INTEGER = 100;
+    const NUM_NOT_INTEGER   = 100;
+    /**
+     * 购买数量必须大于0
+     */
+    const NUM_THAN_ZERO     = 101;
 }
